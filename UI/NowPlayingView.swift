df86a3bd2a7d5819cc4822b6816a43c2ff31821f
Guransh Singh <Guranshsingh7670@gmail.com>
2026-09-08 00:14:48 +0530

    Update NowPlayingView.swift

--- UI/NowPlayingView.swift
+++ UI/NowPlayingView.swift
@@ -86,12 +86,12 @@ struct NowPlayingView: View {
                     .frame(width: size.width, height: size.height)
                     .scaleEffect(1.35)
                     .clipped()
-                    .brightness(isBright ? -0.25 : -0.05)
+                    .brightness(isBright ? -0.15 : -0.05)
                     .saturation(isBright ? 0.8 : 1.45)
                     .blur(radius: isBright ? 45 : 65)
                     .opacity(isBright ? 0.65 : 0.92)
 
-                Color.black.opacity(isBright ? 0.45 : 0.20)
+                Color.black.opacity(isBright ? 0.35 : 0.20)
             } else {
                 Color.black
             }
