2e0987ab95111171281086df14bfb1716107e15e
Guransh Singh <Guranshsingh7670@gmail.com>
2026-09-08 00:37:16 +0530

    Update NowPlayingView.swift

--- UI/NowPlayingView.swift
+++ UI/NowPlayingView.swift
@@ -16,7 +16,7 @@ struct NowPlayingView: View {
 
                 if isLandscape {
                     HStack(spacing: geo.size.width * 0.035) {
-                        artworkPane(maxHeight: geo.size.height * 0.52)
+                        artworkPane(maxHeight: geo.size.height * 0.42)
                             .frame(width: geo.size.width * 0.35)
 
                         lyricsPane
